614ca77f56009415968ca1346cd40ccf9d3ee1e7
Danillo Ferreira <danillodana@outlook.com>
2024-11-22 18:28:53 -0300

    response in application/json

--- src/utils/gen-ai.ts
+++ src/utils/gen-ai.ts
@@ -1,10 +1,38 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"
+import { GoogleGenerativeAI, SchemaType } from "@google/generative-ai"
 
 async function getGenAIRecomendations(book: string) {
   const genAI = new GoogleGenerativeAI(process.env.API_KEY!)
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
 
-  const prompt = `Sugira 5 livros relacionados a "${book}". Retorne apenas em JSON, sem texto adicional.`
+  const schema = {
+    description: "List of recipes",
+    type: SchemaType.ARRAY,
+    items: {
+      type: SchemaType.OBJECT,
+      properties: {
+        title: {
+          type: SchemaType.STRING,
+          description: "Name of the book",
+          nullable: false
+        },
+        author: {
+          type: SchemaType.STRING,
+          description: "Name of the book's author",
+          nullable: false
+        }
+      },
+      required: ["title", "author"]
+    }
+  }
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    generationConfig: {
+      responseMimeType: "application/json",
+      responseSchema: schema
+    }
+  })
+
+  const prompt = `Sugira 5 livros relacionados a "${book}" quero o nome dos autores também.`
 
   const result = await model.generateContent(prompt)
   console.log(result.response.text())
